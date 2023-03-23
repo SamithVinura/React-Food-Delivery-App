3f7805a28a1857b34c0fa5bb154e3ed2b2d35fd8
Samith Vinura <104480599+SamithVinura@users.noreply.github.com>
2023-03-23 08:58:59 +0530

    profile menu and logout done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { AnimatePresence } from "framer-motion";
 
 function App() {
   return (
-    <AnimatePresence>
+    <AnimatePresence mode='wait'>
       <div className="w-screen h-auto flex flex-col bg-primary">
         <Header />
         <main className="mt-14 md:mt-20 px-4 md:px-16 py-4 w-full">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../../src/img/logo.png'
 import { MdShoppingBasket, MdAdd, MdLogout } from "react-icons/md";
-import Avatar from "../components/img/avatar.png";
+import Avatar from "../../src/img/avatar.png";
 import { motion } from "framer-motion";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { app } from '../firebase.config';
@@ -14,14 +14,32 @@ const Header = () => {
   const firebaseAuth  = getAuth(app)
   const provider = new GoogleAuthProvider()
   const [{user},dispatch] =useStateValue()
+  const [isMenu, setIsMenu] = useState(false);
+
 
   const login =async()=>{
-    const { user: { refreshToken, providerData }} = await signInWithPopup(firebaseAuth,provider)
+    if(!user){
+      const { user: { refreshToken, providerData }} = await signInWithPopup(firebaseAuth,provider)
+    
     dispatch({
       type: actionType.SET_USER,
       user: providerData[0],
     })
+    localStorage.setItem('user',JSON.stringify(providerData[0]))
+    }else{
+      setIsMenu(!isMenu);
+    }
   }
+
+  const logout = () => {
+    setIsMenu(false);
+    localStorage.clear();
+
+    dispatch({
+      type: actionType.SET_USER,
+      user: null,
+    });
+  };
   return (
     <header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary">
       <div className="hidden md:flex w-full h-full items-center justify-between">
@@ -31,10 +49,10 @@ const Header = () => {
         </Link>
         <div className="flex items-center gap-8">
           <motion.ul
-            /*   initial={{ opacity: 0, x: 200 }}
+              initial={{ opacity: 0, x: 200 }}
               animate={{ opacity: 1, x: 0 }}
               exit={{ opacity: 0, x: 200 }}
-               */
+              
             className="flex items-center gap-24 "
           >
             <li className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
@@ -64,13 +82,38 @@ const Header = () => {
           <div className="relative">
           <motion.img
               whileTap={{ scale: 0.6 }}
-              src={/* user ? user.photoURL :  */Avatar}
+              src={user ? user.photoURL : Avatar}
               className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full"
               alt="userprofile"
               onClick={login}
             />
           </div>
-          
+          {isMenu && (
+              <motion.div
+                initial={{ opacity: 0, scale: 0.6 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0.6 }}
+                className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0"
+              >
+                {user && user.email === "vinuraaberathne@gmail.com" && (
+                  <Link to={"/createItem"}>
+                    <p
+                      className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
+                      onClick={() => setIsMenu(false)}
+                    >
+                      New Item <MdAdd />
+                    </p>
+                  </Link>
+                )}
+
+                <p
+                  className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
+                  onClick={logout}
+                >
+                  Logout <MdLogout />
+                </p>
+              </motion.div>
+            )}
         </div>
 
       </div>
