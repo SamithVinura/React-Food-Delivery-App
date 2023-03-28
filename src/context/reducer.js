c61c6ff70552fa25c3b8a8e5f49fcbe04229f991
Samith Vinura <104480599+SamithVinura@users.noreply.github.com>
2023-03-28 08:02:28 +0530

    create product function done

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -1,5 +1,8 @@
 export const actionType = {
-     SET_USER : 'SET_USER'
+  SET_USER: "SET_USER",
+  SET_FOOD_ITEMS: "SET_FOOD_ITEMS",
+  SET_CART_SHOW: "SET_CART_SHOW",
+  SET_CARTITEMS: "SET_CARTITEMS",
 }
 
 const reducer = (state,action)=>{
