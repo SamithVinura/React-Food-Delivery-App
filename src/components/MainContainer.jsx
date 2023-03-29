80b88c09d16f991bef3e3e950f454117cb66d0f3
Samith Vinura <104480599+SamithVinura@users.noreply.github.com>
2023-03-29 08:55:53 +0530

    menu container done

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -47,37 +47,15 @@ const MainContainer = () => {
         />
       </section>
 
-      {/*<MenuContainer />
+    <MenuContainer />
 
-      {cartShow && <CartContainer />} */}
+        {/*{cartShow && <CartContainer />} */}
     </div> 
   
 
 
 
-    /* <div className="grid grid-cols-1 md:grid-cols -2 gap-2">
-      <div className="py-2 flex-1 flex flex-col items-start md:items-center justify-center" >
-        <div className='flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full'>
-          <p className='text-base text-orange-500 font-semibold'>Bike Delivery</p>
-          <div className="w-8 h-8 bg-whitel rounded-full overflow-hidden drop-shadow-xl">
-            <img
-              src={Delivery}
-              className="w-full h-full object-contain"
-              alt="delivery" />
-          </div>
-        </div>
-
-        <p className='text-[2.5rem] font-bold tracking-wide text-headingColor'>The Fastest Delivery in <span className='text-orange-600 text-[3rem]'> Your city</span></p>
-        <p className="text-base text-textColor text-center md:text-left md:w-[80%]">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima velit
-          eaque fugit distinctio est nam voluptatum architecto, porro iusto
-          deserunt recusandae ipsa minus eos sunt, dolores illo repellat facere
-          suscipit!
-        </p>
-      </div>
-
-      <div className="py-2 bg-blue-400 flex-1"> </div>
-    </div> */
+    
   )
 }
 
